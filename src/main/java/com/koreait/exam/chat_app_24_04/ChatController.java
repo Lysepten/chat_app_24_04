1c455ea4b032128c4f396b6ea07322042e416379
Lysepten <tlstlsrbrb11@gmail.com>
2024-04-12 13:16:42 +0900

    MessagesResponse 클래스 생성

--- src/main/java/com/koreait/exam/chat_app_24_04/ChatController.java
+++ src/main/java/com/koreait/exam/chat_app_24_04/ChatController.java
@@ -1,9 +1,9 @@
 package com.koreait.exam.chat_app_24_04;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,20 +11,40 @@ import java.util.List;
 @Controller
 @RequestMapping("/chat")
 public class ChatController {
+  private List<ChatMessage> chatMessages = new ArrayList<>();
 
-  private List<ChatMessage> chatMessages = new ArrayList<ChatMessage>();
+  public record writeMessageRequest(String authorName, String content) {
+  }
 
-public record writeMessageResponse(long ID){
+  public record writeMessageResponse(long id) {
 
-}
+  }
 
-  @PostMapping ("/writeMessage")
+  @PostMapping("/writeMessage")
   @ResponseBody
-  public RsData<writeMessageResponse> writeMessage() {
-    ChatMessage message = new ChatMessage("홍길동", "안녕하세요");
-
+  public RsData<writeMessageResponse> writeMessage(@RequestBody writeMessageRequest req) {
+    ChatMessage message = new ChatMessage(req.authorName, req.content);
     chatMessages.add(message);
+    return new RsData<>(
+        "S-1",
+        "메세지가 작성됨",
+        new writeMessageResponse(message.getId()
+        )
+    );
+  }
 
-    return new RsData("S-1", "메세지가 작성됨", new writeMessageResponse(message.getId()));
+  public record MessagesResponse(List<ChatMessage> messages, long count){
+
+  }
+
+  @GetMapping("/messages")
+  @ResponseBody
+  public RsData<MessagesResponse> messages() {
+    return new RsData<>(
+        "S-1",
+        "성공",
+        new MessagesResponse(chatMessages,chatMessages.size())
+    );
+
   }
 }
